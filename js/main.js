66c0e8b4c0ef94a5a227007c7825872b1beefd70
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-12-07 15:15:58 +0100

    Clients opinions

--- js/main.js
+++ js/main.js
@@ -15,4 +15,55 @@ menu.addEventListener('click', function () {
 });
 
 
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRight = document.querySelector('.arrow-right');
+let agencyMember = document.querySelectorAll('.agency-team__member');
+let agencyProfession = document.querySelectorAll('agency-team__job-title');
+let index = 1;
+let array = [];
+
+for (let i = 0; i < agencyMember.length; i++) {
+    array.push(agencyMember[i]);
+}
+
+
+arrowLeft.addEventListener('click', function () {
+    array[index].classList.remove('main-photo');
+    index--;
+    if (index < 0) {
+        index = array.length - 1
+    }
+    array[index].classList.add('main-photo');
+
+});
+
+arrowRight.addEventListener('click', function () {
+    array[index].classList.remove('main-photo');
+    index++;
+    if (index > array.length - 1) {
+        index = 0
+    }
+    array[index].classList.add('main-photo');
+
+});
+
+
+const mainImage = document.querySelector('.agency-clients__main-photo img');
+const smallImages = document.querySelectorAll('.agency-clients__photo');
+const clientOpinion = document.querySelector('.agency-clients__opinion');
+const clientName = document.querySelector('.agency-clients__name');
+
+smallImages.forEach(el => {
+    el.addEventListener('click', function () {
+        let src = el.firstElementChild.getAttribute('src');
+        mainImage.setAttribute('src', src);
+        let quote = el.dataset.quote;
+        let name = el.dataset.name;
+
+        clientOpinion.innerHTML = quote;
+        clientName.innerHTML = name;
+    })
+});
+
+
 
